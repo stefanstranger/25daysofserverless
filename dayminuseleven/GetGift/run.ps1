60e9f48e0c3f291b5085cda7c7cdda1f02e09c20
Stefan Stranger <stefan@stranger.nl>
2019-11-20 19:43:44 +0100

    Initial serverless exercise code

---
+++ dayminuseleven/GetGift/run.ps1
@@ -0,0 +1,37 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger GetGift function processed a request."
+
+# Get TriggerMetadata
+Write-Verbose ($TriggerMetadata | Convertto-Json) -Verbose
+
+Write-Verbose ('Request Object: {0}' -f ($request | convertto-json)) -Verbose
+
+#region read database file
+$Gifts = Get-Content -Path .\GetGift\sample.dat | ConvertFrom-Json
+#endregion
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+if ($name) {
+    $status = [HttpStatusCode]::OK
+    $body = "Hello $($Gifts.name)"
+}
+else {
+    $status = [HttpStatusCode]::BadRequest
+    $body = "Please pass a name on the query string or in the request body."
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $status
+        Body       = $body
+    })
---
+++ dayminuseleven/README.md
@@ -0,0 +1,4 @@
+Little Elsie has a problem.
+
+She wants to tell Santa about the things she wants as gifts, but she is out of both paper, pencils, 
+and stamps to write a quick letter! Wouldn't it be easier if she could create an API to allow Santa to access her requests?
